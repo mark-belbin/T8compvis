49ce09139aabbb4f92a0ea1f2ccc849ca2d66593
Mark Belbin <msbelbin@mun.ca>
2021-02-08 17:37:38 -0330

    Found Intersections
    - Plotted intersecting points with green circles.
    - Used cross product result of the two lines to do this!

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -29,6 +29,11 @@ cv2.imwrite('Dilate.jpg', dilate)
 lines = cv2.HoughLines(edges,0.8,0.005,400) # The parameters are accuracies and threshold
 num = len(lines)
 
+HorizontalLines = []
+VerticalLines = []
+
+# Cycle through Original Line List
+
 for n in range(num):
     rho, theta = lines[n][0]
     a = np.cos(theta)
@@ -44,9 +49,36 @@ for n in range(num):
     if (theta >= pi/2-0.2) and (theta <= pi/2+0.2):
         # Vertical Line = BLUE
         cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+        VerticalLines.append((x1,y1,x2,y2))
     else:
         # Horizontal Line = RED
         cv2.line(img,(x1,y1),(x2,y2),(255,0,0),2)
+        HorizontalLines.append((x1,y1,x2,y2))
 
+# Cycle through Vertical Lines and find intercepts with Horizontal lines
+
+intersects = []
+
+for vertline in VerticalLines:
+    for horzline in HorizontalLines:
+
+        a1 = (vertline[0], vertline[1]) # Point on Vertical Line
+        a2 = (vertline[2], vertline[3]) # Second point on Vertical Line
+
+        b1 = (horzline[0], horzline[1]) # Point on Horizontal Line
+        b2 = (horzline[2], horzline[3]) # Second point on Horizontal Line
+
+        s = np.vstack([a1,a2,b1,b2])        # s for stacked
+        h = np.hstack((s, np.ones((4, 1)))) # h for homogeneous
+        l1 = np.cross(h[0], h[1])           # get first line
+        l2 = np.cross(h[2], h[3])           # get second line
+        x, y, z = np.cross(l1, l2)          # point of intersection
+        if z == 0:                          # lines are parallel
+            break
+        else:
+            intersects.append((int(x/z), int(y/z)))
+
+for point in intersects:
+    img = cv2.circle(img, (point[0], point[1]), radius=20, color=(0,255,0), thickness=-1)
 
 cv2.imwrite('HoughOut.jpg', img)
