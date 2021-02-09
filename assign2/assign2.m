20bf250b4c7f916ff3c332ed13adbb37e6fcdc30
Mark Belbin <msbelbin@mun.ca>
2021-02-09 17:47:25 -0330

    It Works!
    - It's beautiful, so beautiful

--- assign2/assign2.m
+++ assign2/assign2.m
@@ -8,33 +8,33 @@ clear
 
 % ~~~~~~ Hough inputs ~~~~~~~~~~~ %
 r_max = 120;   % Maximum circle radius                 
-thresh = 1000;  % Hough accumulator threshold    
+thresh = 25;  % Hough accumulator threshold
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
 
 
 % Import images and convert to grayscale %
 im1_rgb = imread('im1.png');
 im2_rgb = imread('im2.png');
-im1 = rgb2gray(im1_rgb);
-im2 = rgb2gray(im2_rgb);
-im2 = im2(1:469, 1:625); % Crop im2 to match im1 dimensions
+im1_gray = rgb2gray(im1_rgb);
+im2_gray = rgb2gray(im2_rgb);
+im2_gray = im2_gray(1:469, 1:625); % Crop im2 to match im1 dimensions
 
 % Height and width of im1/im2
-height = size(im1, 1);
-width = size(im1, 2);
+height = size(im1_gray, 1);
+width = size(im1_gray, 2);
 
 % Plot Original grayscale images
 figure;
 subplot(121);
-imshow(im1);
+imshow(im1_gray);
 title('GrayScale Im1');
 subplot(122);
-imshow(im2);
+imshow(im2_gray);
 title('GrayScale Im1');
 
 % Apply gaussian filter
-im1 = imgaussfilt(im1, 7.0);
-im2 = imgaussfilt(im2, 3.0);
+im1 = imgaussfilt(im1_gray, 7.0);
+im2 = imgaussfilt(im2_gray, 3.0);
 
 % Put images through canny edge detector
 im1 = edge(im1, 'canny');
@@ -50,56 +50,32 @@ subplot(122);
 imshow(im2);
 title('Canny Im2');
 
-a = width; % x0
-b = height; % y0
-r = r_max; %r
+% Perform Custom Hough Circle Transform on Im1
+circles1 = HoughCircles(im1, r_max, thresh);
 
-A_im1 = zeros(a,b,r); % 3D Accumulator Array
+% Perform Custom Hough Circle Transform on Im1
+circles2 = HoughCircles(im2, r_max, thresh);
 
-% Accumulator Lööp for im1
-for r = 1:1:r_max
-    for y = 1:1:height
-        for x = 1:1:width
-            if im1(y,x) == 1
-                for theta = 0:0.1:2*pi
-                    % Calculate circle pixel locations
-                    a = uint8(x - r * cos(theta));
-                    b = uint8(y - r * sin(theta));
-                    
-                    % Check if circle is within accumulator bounds
-                    if (a < 1) || (a > width)
-                        break
-                    elseif (b < 1) || (b > height)
-                        break
-                    else
-                        A_im1(a,b,r) = A_im1(a,b,r) + 1; % Add to accumulator
-                    end
-                end
-            end
-        end
-    end
-end
+% Draw Circles
+im1_rgb = insertShape(im1_rgb,'circle',circles1,'LineWidth',3,'Color','red');
+im2_rgb = insertShape(im2_rgb,'circle',circles2,'LineWidth',3,'Color','red');
 
-% Find local maxima in 3D array
-maxima = islocalmax(A_im1);
-
-circle_count = 0;
-for r = 1:1:r_max
-    for y = 1:1:height
-        for x = 1:1:width
-            if (maxima(x,y,r) == 1) && (A_im1(x,y,r) > thresh)
-               circle_count = circle_count + 1;
-               im1_rgb = insertShape(im1_rgb,'circle',[x y r],'LineWidth',3,'Color','red');
-            end
-        end
-    end
-end
-
-disp(circle_count)
-
-% Plot Result image
+% Plot Result image for im1
 figure;
+subplot(121);
+imshow(im1_gray);
+title('Im1 Original');
+subplot(122);
 imshow(im1_rgb);
 title('Im1 With Detected Circles');
+
+% Plot Result image for im2
+figure;
+subplot(121);
+imshow(im2_gray);
+title('Im2 Original');
+subplot(122);
+imshow(im2_rgb);
+title('Im2 With Detected Circles');
 
 
